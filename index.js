06f819b14f89a976bc5537c3a2a9a36189fd03e8
nivaldopinheiro <2004364@aluno.univesp.br>
2022-11-29 19:35:56 -0300

    projetointegrador3.1.1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser') ;
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser') ;
+const app = express();
 //const handlebars = require('express-handlebars');
-var PORT = 3000;
+const PORT = process.env.port || 3000;
 
 //carregado helps de logado 
 
